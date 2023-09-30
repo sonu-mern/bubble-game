48089c6417ce50927e957d3d9d0d1bd7ad5b0763
sonu-mern <mahtosonurajkumar108@gmail.com>
2023-09-30 20:14:56 +0530

    Initial commit

---
+++ js.js
@@ -0,0 +1,87 @@
+var timer = 60;
+var num = 0;
+var ran = 0;
+var newscore=0;
+var newscore=0;
+function start (){
+
+    function makeBubble(){
+        var clutter ="";
+    
+        for(var i = 1;i<=189;i++){
+             ran = Math.floor(Math.random()*10);
+            clutter += `<div class="bubble">${ran}</div>`;
+        
+        }
+        document.querySelector(".pbottom").innerHTML= clutter;
+    }
+    makeBubble();
+    
+    function runtimer(){
+    var tcount=   setInterval(function (){
+           if(timer > 0){
+    
+               timer--;
+               document.querySelector("#time").textContent = timer;
+           }
+           else{
+               clearInterval(tcount);
+               document.querySelector(".pbottom").innerHTML=`<h1>Game Over</h1>`
+               document.querySelector("#hit").textContent=0;
+    
+           }
+    
+       },1000);
+    }
+    runtimer();
+    
+    function hitcount(){
+         num = Math.floor(Math.random()*10);
+        document.querySelector("#hit").innerHTML=num;
+    }
+    hitcount();
+    
+    function nscore(){
+       
+        newscore += 10;
+        document.querySelector("#score").textContent=newscore;
+    }
+    function mscore(){
+       
+        mnewscore += 10;
+        document.querySelector("#score").textContent=mnewscore;
+    }
+    
+    function countScore (){
+      document.querySelector(".pbottom").addEventListener("click",function(detail){
+            var clickedNum = Number(detail.target.textContent);
+            const audioElement = document.getElementById("myAudio");
+           
+                // Check if the audio is currently paused or ended
+                if (audioElement.paused || audioElement.ended) {
+                    // Play the audio
+                    audioElement.play();
+                } else {
+                    // If the audio is already playing, pause and rewind it to the beginning
+                    audioElement.pause();
+                    audioElement.currentTime = 0;
+                }
+            ;
+          
+            if(clickedNum===num){
+               nscore();
+               makeBubble();
+               hitcount();
+            }else{
+
+            }
+        })
+    
+    }
+    countScore();
+}
+
+
+
+
+
